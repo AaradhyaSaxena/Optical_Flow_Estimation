e4db552362b583e2af457829e761b2c7da065b0d
AaradhyaSaxena <aaradhya271197@gmail.com>
2019-06-23 19:36:27 +0530

    w6_7

---
+++ gen_data.py
@@ -0,0 +1,94 @@
+import cv2
+import os
+import numpy as np
+from natsort import natsorted
+import gc
+import  matplotlib.pyplot as plt
+import sys
+import pexpect
+from subprocess import call
+import shutil
+from  scipy.misc import  imsave,imread
+import scipy.io as sio
+
+
+
+
+#read pfm file which we get from flow net 
+def readFlow(name):
+    if name.endswith('.pfm') or name.endswith('.PFM'):
+        return readPFM(name)[0][:,:,0:2]
+
+    f = open(name, 'rb')
+
+    header = f.read(4)
+    if header.decode("utf-8") != 'PIEH':
+        raise Exception('Flow file header does not contain PIEH')
+
+    width = np.fromfile(f, np.int32, 1).squeeze()
+    height = np.fromfile(f, np.int32, 1).squeeze()
+
+    flow = np.fromfile(f, np.float32, width * height * 2).reshape((height, width, 2))
+
+    flow=flow.astype(np.float32)
+    flow[flow>10000]=0
+    return flow
+
+
+def get_images(dirx):
+    l=os.listdir(dirx)
+    frame1=imread(dirx+"/"+'frame10.png')
+    frame2=imread(dirx+"/"+'frame11.png')
+    return [frame1,frame2]
+
+
+def get_flow(diry):
+    l=os.listdir(diry)
+    flow=readFlow(diry+"/"+l[0])
+    return flow
+
+  
+def get_data():
+	DATA_DIR_X ="/home/ranjan/work/flow/data/middlebury_color/other-data/"
+	DATA_DIR_GT ="/home/ranjan/work/flow/data/middlebury_color/other-gt-flow/"
+	d=os.listdir(DATA_DIR_GT)
+	X=[]
+	Y=[]
+	for i in range(len(d)):
+	    dirx=DATA_DIR_X + d[i]
+	    diry=DATA_DIR_GT + d[i]
+	    #print os.listdir(dirx),os.listdir(diry)
+	    x_temp=get_images(dirx)
+	    y_temp=get_flow(diry)
+	    X.append(x_temp)
+	    Y.append(y_temp)
+
+	X=np.array(X)
+	Y=np.array(Y)
+
+	return X, Y
+
+
+def gen_train_data(shape=(100,100),STRIDE=(50,50)):
+    X,Y=get_data()
+
+    PLIST=[] #patch list of 2 consecutive patch
+    FLIST=[] #flow list
+    for i in range(Y.shape[0]):
+	idx=np.r_[:Y[i].shape[0]-shape[0]:STRIDE[0]]
+	idy=np.r_[:Y[i].shape[1]-shape[1]:STRIDE[1]]
+	for idx1 in idx:
+	    for idy1 in idy:    
+                patch1=X[i][0][idx1:idx1+shape[0],idy1:idy1+shape[1],:]		
+                patch2=X[i][1][idx1:idx1+shape[0],idy1:idy1+shape[1],:]
+	    #print patch1.shape,patch2.shape	
+		flow=Y[i][idx1:idx1+shape[0],idy1:idy1+shape[1],:]
+
+   		PLIST.append([patch1,patch2])
+   		FLIST.append(flow)
+
+    PLIST=np.array(PLIST)
+    FLIST=np.array(FLIST)
+
+
+
---
+++ generator.py
@@ -0,0 +1,92 @@
+# coding=utf-8
+import skimage.transform
+from keras.utils import Sequence
+import numpy as np
+import cv2
+import glob
+import pandas as pd
+import os
+from scipy import misc
+from skimage.transform import  resize
+from init import *
+
+def rgbf2bgr(rgbf):
+	t = rgbf*255.0
+	t = np.clip(t, 0.,255.0)
+	bgr = t.astype(np.uint8)[..., ::-1]
+	return bgr
+
+def rgbf2rgb(rgbf):
+	t = rgbf*255.0
+	t = np.clip(t, 0.,255.0)
+	rgb = t.astype(np.uint8)
+	return rgb
+
+class ImageSequence(Sequence):
+
+    def __init__(self,  batch_size=4, input_size=(240,360), f_gap=1):
+        self.image_seq_path="/home/ranjan/work/flow/data/Test/eval-data-gray/"
+        #self.image_seq_path='../data/UCSD/UCSDped1/Train/'
+        self.max_sence= 36
+        self.input_shape=input_size
+        self.dirs=os.listdir(self.image_seq_path)
+        self.dirs.sort()
+        self.len_dirs=len(self.dirs)
+        self.batch_size = batch_size
+        self.epoch = 0
+        self.f_gap=f_gap
+        self.SHAPE_Y=self.input_shape[0]
+        self.SHAPE_X=self.input_shape[1]
+
+    def __len__(self):
+        return (180)
+
+    def read_image(self,file_path):
+        Img=misc.imread(file_path)
+        Img=resize(Img,(SHAPE_Y,SHAPE_X))
+        return Img
+
+    def __getitem__(self, idx):
+
+        x_batch = []
+        c=0
+        d_idx=np.random.randint(0,self.len_dirs)  #sence IDX
+        path=self.image_seq_path+self.dirs[d_idx]+"/"
+        self.frames=os.listdir(path)
+        self.frames.sort()
+        num_frames=len(self.frames)
+
+        while(c<self.batch_size):
+                              
+            s_idx =np.random.randint(0,num_frames-self.f_gap)  #sence IDX
+                              
+            I1_file=self.frames[s_idx]  
+            I2_file=self.frames[s_idx+self.f_gap]
+            c=c+1
+            I1=self.read_image(path+I1_file)
+            I2=self.read_image(path+I2_file)
+            # I1=resize(I1,(self.SHAPE_Y,self.SHAPE_X,1))
+            # I2=resize(I2,(self.SHAPE_Y,self.SHAPE_X,1))
+            I1=np.expand_dims(I1,axis=-1) 
+            I2=np.expand_dims(I2,axis=-1) 
+            x_batch.append([I1,I2])
+
+        x_batch = np.array(x_batch, np.float32)
+        # x_batch=np.zeros((4,2,240, 360, 1))
+        # return ([x_batch[:,0,:,:,:],x_batch[:,1,:,:,:]],y_batch)
+        x_batch1=x_batch[:,0,:SHAPE_Y,:SHAPE_X,:]
+        x_batch2=x_batch[:,1,:SHAPE_Y,:SHAPE_X,:]
+
+        #return ([x_batch[:,0,:,:,:],x_batch[:,1,:,:,:]],None)
+        return ([x_batch1,x_batch2],None)
+        #return x_batch
+
+    def on_epoch_end(self):
+        self.epoch += 1
+
+
+"""
+from generator import *
+A=ImageSequence()
+A.__getitem__(3)
+"""
