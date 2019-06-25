6bab9eb91c84e4f285ce7a19ae4c333462135bcf
AaradhyaSaxena <aaradhya271197@gmail.com>
2019-06-26 00:01:12 +0530

    w7_2

--- generator.py
+++ generator.py
@@ -24,13 +24,11 @@ def rgbf2rgb(rgbf):
 	return rgb
 
 
-
-
 class ImageSequence(Sequence):
-    def __init__(self,  batch_size=4, input_size=(240, 360),f_gap=1):
-        self.image_seq_path="/home/ranjan/work/flow/data/Test/eval-data-gray/"
+    def __init__(self,  batch_size=4, input_size=(436, 1024),f_gap=1):
+        self.image_seq_path="/media/newhd/data/flow/MPI_SINTEL/MPI-Sintel-complete/training/albedo"
         #self.image_seq_path='../data/UCSD/UCSDped1/Train/'
-        self.max_sence= 36
+        self.max_sence= 50                      # whats this for?
         self.input_shape=input_size
         self.dirs=os.listdir(self.image_seq_path)
         self.dirs.sort()
@@ -42,7 +40,7 @@ class ImageSequence(Sequence):
         self.SHAPE_X=self.input_shape[1]
 
 
-    def __len__(self):
+    def __len__(self):                     
         return (180)
 
     def read_image(self,file_path):
@@ -52,34 +50,33 @@ class ImageSequence(Sequence):
 
     def __getitem__(self, idx):
         x_batch = []
-        c=0
-        d_idx =np.random.randint(0,self.len_dirs)  #sence IDX
-        path=self.image_seq_path+self.dirs[d_idx]+"/"
-        self.frames=os.listdir(path)
+        c = 0
+        d_idx = np.random.randint(0,self.len_dirs) 
+        path = self.image_seq_path+self.dirs[d_idx]+"/"
+        self.frames = os.listdir(path)
         self.frames.sort()
-        num_frames=len(self.frames)
+        num_frames = len(self.frames)
 
         while(c<self.batch_size):
                               
-            s_idx =np.random.randint(0,num_frames-self.f_gap)  #sence IDX
-                              
-            I1_file=self.frames[s_idx]  
-            I2_file=self.frames[s_idx+self.f_gap]
-            c=c+1
+            s_idx = np.random.randint(0,num_frames-self.f_gap)  #sence IDX  ????????????
+                             
+            I1_file = self.frames[s_idx]  
+            I2_file = self.frames[s_idx+self.f_gap]
+            c = c+1
             I1=self.read_image(path+I1_file)
             I2=self.read_image(path+I2_file)
             #I1=resize(I1,(self.SHAPE_Y,self.SHAPE_X,1))                
             #I2=resize(I2,(self.SHAPE_Y,self.SHAPE_X,1))               
-	    I1=np.expand_dims(I1,axis=-1) 
-	    I2=np.expand_dims(I2,axis=-1) 
+            I1=np.expand_dims(I1,axis=-1) 
+            I2=np.expand_dims(I2,axis=-1) 
             x_batch.append([I1,I2])
-
 
         x_batch = np.array(x_batch, np.float32)
         #x_batch=np.zeros((4,2,240, 360, 1))
         #return ([x_batch[:,0,:,:,:],x_batch[:,1,:,:,:]],y_batch)
-	x_batch1=x_batch[:,0,:SHAPE_Y,:SHAPE_X,:]
-	x_batch2=x_batch[:,1,:SHAPE_Y,:SHAPE_X,:]
+        x_batch1=x_batch[:,0,:SHAPE_Y,:SHAPE_X,:]
+        x_batch2=x_batch[:,1,:SHAPE_Y,:SHAPE_X,:]
 
         #return ([x_batch[:,0,:,:,:],x_batch[:,1,:,:,:]],None)
         return ([x_batch1,x_batch2],None)
@@ -89,8 +86,8 @@ class ImageSequence(Sequence):
         self.epoch += 1
 
 
-"""
+
 from generator import *
 A=ImageSequence()
 A.__getitem__(3)
-"""
+
