b6548cc6b317bf6ab44a54b22d28fa43a5a98fbc
AaradhyaSaxena <aaradhya271197@gmail.com>
2019-06-28 00:39:05 +0530

    old_updated

--- sample1.py
+++ sample1.py
@@ -1,8 +1,8 @@
 from keras import Input, Model
 from keras.layers import Conv2D, BatchNormalization, MaxPooling2D, UpSampling2D, Add, Dropout, concatenate,Flatten,Dense
 from keras.layers import Lambda,Reshape,LocallyConnected2D,SeparableConv1D,LocallyConnected1D,LeakyReLU
-# import os
-# os.environ['CUDA_VISIBLE_DEVICES'] ='-1'
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] ='0'
 from keras.utils import plot_model
 import keras.backend as K
 from keras.layers import Input,Dense,Conv2D,MaxPooling2D,UpSampling2D,Conv2DTranspose
@@ -30,8 +30,11 @@ import keras_contrib.backend as KC
 from keras.utils import multi_gpu_model
 import tensorflow as tf
 from generator import *
-
 from model import *
+from loss import *
+
+###-----------------Model-----------------------
+
 def return_model(shape=(436,1024,3)):
 
     I1 = Input(shape=shape)
@@ -50,20 +53,24 @@ def return_model(shape=(436,1024,3)):
     z = Conv2D(2,(5,5), padding='same',activation="relu")(z)
 
     model = Model(inputs=[I1,I2], outputs=[z])
+    model.compile(loss=c_mseAbs,optimizer="Adam")
     # model.compile(loss="mse",optimizer="Adam")
-    model.compile(loss=DSSIMObjective(kernel_size=50),optimizer="Adam")
+    # model.compile(loss=DSSIMObjective(kernel_size=75),optimizer="Adam")
+
+
     return model
 ##------------------------------------------
 
-#model=return_model()
-model=create_model(input_shape=(436,1024,3))
-model.compile(loss="mse",optimizer="Adam")
+model=return_model()
+
+# model=create_model(input_shape=(436,1024,3))
+# model.compile(loss="mse",optimizer="Adam")
+
 #-------------------DATA-------------------
+
 # X1 = np.random.rand(100,436,1024,3)
 # X2 = np.random.rand(100,436,1024,3)
 # y = np.random.rand(100,436,1024,2)
-
-
 
 
 imgen=ImageSequence_new()
@@ -74,11 +81,16 @@ imgen=ImageSequence_new()
 
 
 # model.fit_generator(imgen,epochs=100)
-model.fit([X1,X2],Y,batch_size=4,epochs=10000)
+
+model.fit([X1,X2],Y,batch_size=4,epochs=100)
 # model.load_weights('data/model_1')
 
 y=model.predict([X1,X2])
 
-model.save_weights("data/model.h5")
+model.save_weights("data/temp.h5")
 
 # np.savez('sample_model1', flow =y1)
+
+####--------------viz-------------------
+# %matplotlib
+# plt.imshow(y[0,:,:,0])
